471f29d553613bfdc261963a5767b35c42740e81
ysknsid25 <kengo071225@gmail.com>
2023-05-13 11:15:07 +0900

    chore: tailwindの設定
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  darkMode: false, // 'media' or 'class',
   purge: {
     content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+    options: {
+      // https://purgecss.com/safelisting.html#patterns
+      safelist: {
+        standard: [/^bg-/, /^text-/],
+      },
+    },
   },
+  content: [],
   theme: {
     extend: {},
   },
