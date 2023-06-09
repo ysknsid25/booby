78901ed31ec73e8cb810ac9b764ccf847686af49
jota <funayama.jota@gmail.com>
2023-06-10 00:46:26 +0900

    test: paginationのテストを修正

--- src/Components/__test__/pagination.test.tsx
+++ src/Components/__test__/pagination.test.tsx
@@ -10,41 +10,37 @@ jest.mock('next/router', () => ({
 }))
 
 describe('Paginationのテスト', () => {
-  test('Paginationのページ数が5以下の場合にlinkの数がページと同じように表示されているかどうか', () => {
+  test('Paginationのページ数が6以下の場合にlinkの数がページと同じように表示されているかどうか', () => {
     const totalCount = 100;
     const perPage = 50;
     const totalPage = Math.ceil(totalCount / perPage); // 2
     const { getAllByRole } = render(<Pagination currentPage={1} totalCount={totalCount} perPage={perPage} />)
     expect(getAllByRole('link')).toHaveLength(totalPage)
   })
-  test('Paginationのページ数が6以上の場合に「...」が表示されているかどうか', () => {
-    const totalCount = 300
+  test('Paginationのページ数が7以上の場合に「...」が表示されているかどうか', () => {
+    const totalCount = 350
     const perPage = 50
-    const totalPage = Math.ceil(totalCount / perPage) // 6
+    const totalPage = Math.ceil(totalCount / perPage) // 7
     const { getByText } = render(<Pagination currentPage={1} totalCount={totalCount} perPage={perPage} />)
     expect(getByText('...')).toBeInTheDocument()
   })
-  test('Paginationのページ数が6以上の場合にlinkの数が4つ表示されているかどうか', () => {
-    const totalCount = 251
+  test('Paginationのページ数が7以上の場合にlinkの数が5つ表示されているかどうか', () => {
+    const totalCount = 301
     const perPage = 50
-    const totalPage = Math.ceil(totalCount / perPage) // 6
+    const totalPage = Math.ceil(totalCount / perPage) // 7
     const { getAllByRole } = render(<Pagination currentPage={1} totalCount={totalCount} perPage={perPage} />)
-    expect(getAllByRole('link')).toHaveLength(4)
-  })
-  test('Paginationのページ数が6以上の場合にlinkの数が4つ表示されているかどうか', () => {
-    const totalCount = 251
-    const perPage = 50
-    const totalPage = Math.ceil(totalCount / perPage) // 6
-    const { getAllByRole } = render(<Pagination currentPage={1} totalCount={totalCount} perPage={perPage} />)
-    expect(getAllByRole('link')).toHaveLength(4)
+    expect(getAllByRole('link')).toHaveLength(5)
   })
   test('PaginationのtotalCountが1000を超える場合は1000を超えるアイテムが表示されるページへの遷移をしないようにする', () => {
     const totalCount = 2000
     const perPage = 50
-    const { getAllByRole } = render(<Pagination currentPage={1} totalCount={totalCount} perPage={perPage} />)
+    const { getAllByRole, queryByText } = render(
+      <Pagination currentPage={20} totalCount={totalCount} perPage={perPage} />,
+    )
     const links = getAllByRole('link')
     const lastLink = links[links.length - 1]
     expect(lastLink.textContent).toBe('20')
+    expect(queryByText('21')).toBeNull();
   })
   test('Paginationの現在のページのボタンがアクティブ表記されているか', () => {
     const totalCount = 500
@@ -57,12 +53,10 @@ describe('Paginationのテスト', () => {
     const totalCount = 500
     const perPage = 50
     const totalPage = Math.ceil(totalCount / perPage) // 10
-    const { getAllByRole } = render(
-      <Pagination currentPage={1} totalCount={totalCount} perPage={perPage} />,
+    const { getByText } = render(
+      <Pagination currentPage={8} totalCount={totalCount} perPage={perPage} />,
     )
-    const links = getAllByRole('link')
-    const lastLink = links[links.length - 1]
-    expect(lastLink.textContent).toBe('10')
-    expect(lastLink).toHaveAttribute('href', '/?language=javascript&page=10')
+    const link = getByText('9')
+    expect(link).toHaveAttribute('href', '/?language=javascript&page=9')
   })
 })
