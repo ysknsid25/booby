dda5607b51d9ef6490598ac4274c95f3341ee5dd
jota <funayama.jota@gmail.com>
2023-06-10 00:23:06 +0900

    update: ページ番号の表示方法を変更

--- src/Components/pagination.tsx
+++ src/Components/pagination.tsx
@@ -7,6 +7,23 @@ type Props = {
   perPage: number
 }
 
+function getPageNumbers(currentPage: number, totalPage: number): Array<string | number> {
+  // 6ページ以下の場合は全て表示する
+  if (totalPage <= 6) {
+    return Array.from({ length: totalPage }, (_, index) => index + 1)
+  }
+
+  if(currentPage <= 3) {
+    return [1, 2, 3, 4, 5, '...']
+  }
+
+  if(currentPage >= totalPage - 2) {
+    return ['...', totalPage - 4, totalPage - 3, totalPage - 2, totalPage - 1, totalPage]
+  }
+
+  return ['...', currentPage - 2, currentPage - 1, currentPage, currentPage + 1, currentPage + 2, '...']
+}
+
 export default function Pagination({ currentPage, totalCount, perPage }: Props) {
   const router = useRouter()
   let totalPage = Math.ceil(totalCount / perPage)
@@ -14,21 +31,15 @@ export default function Pagination({ currentPage, totalCount, perPage }: Props) 
   if (totalCount > 1000) {
     totalPage = Math.floor(1000 / perPage)
   }
-  // 5ページ以下の場合は全て表示する
-  let pageNumbers: Array<string | number> = []
-  if (totalPage <= 5) {
-    pageNumbers = Array.from({ length: totalPage }, (_, index) => index + 1)
-  } else {
-    pageNumbers = [1, 2, '...', totalPage - 1, totalPage]
-  }
+  let pageNumbers: Array<string | number> = getPageNumbers(currentPage, totalPage)
   return (
     <div className='flex items-center justify-center bg-white px-4 py-3 sm:px-6'>
       <nav className='inline-flex gap-x-1.5' aria-label='Pagination'>
-        {pageNumbers.map((pageNumber) => {
+        {pageNumbers.map((pageNumber, i) => {
           if (pageNumber === '...') {
             return (
               <span
-                key={pageNumber}
+                key={i}
                 className='w-10 h-10 rounded-full inline-flex items-center text-lg justify-center'
               >
                 ...
@@ -38,7 +49,7 @@ export default function Pagination({ currentPage, totalCount, perPage }: Props) 
           if (pageNumber === currentPage) {
             return (
               <Link
-                key={pageNumber}
+                key={i}
                 href={{ pathname: router.pathname, query: { ...router.query, page: pageNumber } }}
                 className='w-10 h-10 rounded-full inline-flex items-center justify-center bg-indigo-600 text-sm font-semibold text-white'
               >
@@ -48,7 +59,7 @@ export default function Pagination({ currentPage, totalCount, perPage }: Props) 
           }
           return (
             <Link
-              key={pageNumber}
+              key={i}
               href={{ pathname: router.pathname, query: { ...router.query, page: pageNumber } }}
               className='w-10 h-10 rounded-full inline-flex items-center justify-center ring-1 ring-inset ring-gray-300 text-sm'
             >
